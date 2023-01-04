d45a79145b2e814d16fe4315268a6f353b9cdae9
royggle <yunoroy@gmail.com>
2023-01-05 07:16:24 +0900

    [ADD] create python calculator assignment

---
+++ calculator.py
@@ -0,0 +1,19 @@
+playing = True
+
+while playing == True:
+  a = int(input("Choose a number:\n"))
+  b = int(input("Choose another one:\n"))
+  operation = input("Choose an operation:\n    Options are: + , - , * or /.\n    Write 'exit' to finish.\n")
+    
+  # add your code here!
+
+  if operation == "+":
+    print(a+b)
+  elif operation == "-":
+    print(a-b)
+  elif operation == "*":
+    print(a*b)
+  elif operation == "/":
+    print(a/b)
+  elif operation == "exit":
+    playing = False
--- job_scrapper.py
+++ job_scrapper.py
@@ -1,4 +1,5 @@
 from requests import get
+from bs4 import BeautifulSoup
 
 base_url = 'http://weworkremotely.com/remote-jobs/search?term='
 search_term = 'python'
@@ -7,4 +8,13 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
   print("Can't request website")
 else:
-  print(response.text)
+  soup = BeautifulSoup(response.text, "html.parser")
+  # print(soup.findAll("section", class_="jobs"))
+  jobs = soup.find_all("section", class_="jobs")
+  print(len(jobs))
+  for job_section in jobs:
+    job_posts = job_section.find_all('li')
+    for post in job_posts:
+      print(post)
+      print("///////////////////////")
+        
