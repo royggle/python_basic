869fce9258f1be860f81968dd13e27855f1e6625
royggle <yunoroy@gmail.com>
2023-01-06 21:10:36 +0900

    [MODIFY] update on job scrapper

--- job_scrapper.py
+++ job_scrapper.py
@@ -8,13 +8,28 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
   print("Can't request website")
 else:
+  results = []
   soup = BeautifulSoup(response.text, "html.parser")
   # print(soup.findAll("section", class_="jobs"))
   jobs = soup.find_all("section", class_="jobs")
   print(len(jobs))
   for job_section in jobs:
     job_posts = job_section.find_all('li')
+    job_posts.pop(-1)
     for post in job_posts:
-      print(post)
-      print("///////////////////////")
-        
+      anchors = post.find_all('a')
+      anchor = anchors[1]
+      link = anchor['href']
+      company, kind, region = anchor.find_all('span', class_="company")
+      
+      title = anchor.find('span', class_="title")
+      job_data = {
+        'company': company.string,
+        'region': region.string,
+        'position':title.string,
+        'kind': kind.string,
+      }
+      results.append(job_data)
+  for result in results:
+    print(result)
+    print("--------------------------------")
