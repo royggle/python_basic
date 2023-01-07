1baa1b473617a21aa276b8ec23859db303d55cfa
royggle <yunoroy@gmail.com>
2023-01-07 09:04:53 +0900

    [ADD] create sum only numbers assignment by using for

--- for_phrase_assignment.py
+++ for_phrase_assignment.py
@@ -1,11 +1,8 @@
-food = ('🍔', '🍕', '🍌', '🍎', '🥑')
-
-
-print(food[-4])
-
-
 numbers = [1, "💖", 2, "🔥", 3, "⭐️", 4, "💖", 5, "🔥", 6, "⭐️", 7, "💖", 8, "🔥", 9, "⭐️", 10, "💖", 11, "🔥", 12, "⭐️", 13, "💖", 14, "🔥", 15, "⭐️", 16]
-
-numeric = "💖"
-
-print(type(numeric)== str)
+sum = 0
+for number in numbers:
+  if type(number) == str:
+    continue
+  else:
+    sum += number
+print(sum)
